1cc5292816782227a551d613e40a143c38f0f838
Pama <625706468@qq.com>
2018-01-22 13:12:24 +0800

    计算器 更新

--- src/resource/component/calc/Calc.js
+++ src/resource/component/calc/Calc.js
@@ -2,189 +2,108 @@ import Btn from 'componentDir/calc/Btn.js'
 
 require('cssDir/calc/index.css')
 
-const calcFunc = (temporary, sum, symbol) => {
-	switch(symbol) {
-		case "%":
-			return Number(sum) * 0.01
-		break
-		case "+":
-			return Number(temporary) + Number(sum)
-		break
-		case "-":
-			return Number(temporary) - Number(sum)
-		break
-		case "×":
-			return Number(temporary) * Number(sum)
-		break
-		case "÷":
-			return Number(temporary) / Number(sum)
-		break
-	}
-}
+const log = console.log.bind(console)
+const btnList = [
+	{
+		val: "C",
+		style: "btn-gray",
+	},
+	{
+		val: "+/-",
+		style: "btn-gray",
+	},
+	{
+		val: "%",
+		style: "btn-gray",
+	},
+	{
+		val: "÷",
+		style: "btn-orange",
+	},
+	{
+		val: "7",
+		style: "btn-black",
+	},
+	{
+		val: "8",
+		style: "btn-black",
+	},
+	{
+		val: "9",
+		style: "btn-black",
+	},
+	{
+		val: "×",
+		style: "btn-orange",
+	},
+	{
+		val: "4",
+		style: "btn-black",
+	},
+	{
+		val: "5",
+		style: "btn-black",
+	},
+	{
+		val: "6",
+		style: "btn-black",
+	},
+	{
+		val: "-",
+		style: "btn-orange",
+	},
+	{
+		val: "1",
+		style: "btn-black",
+	},
+	{
+		val: "2",
+		style: "btn-black",
+	},
+	{
+		val: "3",
+		style: "btn-black",
+	},
+	{
+		val: "+",
+		style: "btn-orange",
+	},
+	{
+		val: "0",
+		style: "btn-black-special",
+	},
+	{
+		val: ".",
+		style: "btn-black",
+	},
+	{
+		val: "=",
+		style: "btn-orange",
+	},
+]
+const numArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 class Calc extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			sum: 0,
-			temporary: 0,
+			num: 0,
 			symbol: null,
 		}
-		this.handleClear = this.handleClear.bind(this)
-		this.handleNegate = this.handleNegate.bind(this)
-		this.handleNum = this.handleNum.bind(this)
-		this.handleSymbol = this.handleSymbol.bind(this)
-		this.handlePercent = this.handlePercent.bind(this)
-		this.handleResult = this.handleResult.bind(this)
+		this.handleClick = this.handleClick.bind(this)
 	}
-	handleClear() {
-		this.setState({
-			sum: 0,
-			temporary: 0,
-			symbol: null,
-		})
-	}
-	handleNegate() {
-		this.setState({
-			sum: - this.state.sum,
-		})
-	}
-	handleNum(e) {
-		console.log(this.state)
-		const html = e.target.innerHTML
-		if (typeof this.state.sum !== "number") {
-			this.setState({
-				sum: this.state.sum + html,
-			})
-		} else {
-			html !== "0" ? (html !== '.' ? this.setState({ sum: html }) : this.setState({ sum: 0 + html })) : this.setState({ sum: 0 })
-		}
-	}
-	handleSymbol(e) {
-		this.setState({
-			sum: Number(this.state.sum),
-			temporary: Number(this.state.sum),
-			symbol: e.target.innerHTML
-		})
-	}
-	handlePercent() {
-		this.setState({
-			sum: calcFunc(this.state.temporary, this.state.sum, "%"),
-			temporary: this.state.sum,
-		})
-	}
-	handleResult() {
-		console.log(typeof calcFunc(this.state.temporary, this.state.sum, this.state.symbol))
-		this.setState({
-			sum: calcFunc(this.state.temporary, this.state.sum, this.state.symbol),
-		})
+	handleClick(e) {
+		log(e.target.innerHTML)
 	}
 	render() {
-		const btnList = [
-			{
-				val: "C",
-				style: "btn-gray",
-				click: this.handleClear,
-			},
-			{
-				val: "+/-",
-				style: "btn-gray",
-				click: this.handleNegate,
-			},
-			{
-				val: "%",
-				style: "btn-gray",
-				click: this.handlePercent,
-			},
-			{
-				val: "÷",
-				style: "btn-orange",
-				click: this.handleSymbol,
-			},
-			{
-				val: "7",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "8",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "9",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "×",
-				style: "btn-orange",
-				click: this.handleSymbol,
-			},
-			{
-				val: "4",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "5",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "6",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "-",
-				style: "btn-orange",
-				click: this.handleSymbol,
-			},
-			{
-				val: "1",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "2",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "3",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "+",
-				style: "btn-orange",
-				click: this.handleSymbol,
-			},
-			{
-				val: "0",
-				style: "btn-black-special",
-				click: this.handleNum,
-			},
-			{
-				val: ".",
-				style: "btn-black",
-				click: this.handleNum,
-			},
-			{
-				val: "=",
-				style: "btn-orange",
-				click: this.handleResult,
-			},
-		]
 		const btnArray = btnList.map((val, index) => {
 			return (
-				<Btn style={ val.style } val={ val.val } click={ val.click } key={ index } />
+				<Btn style={ val.style } val={ val.val } click={ this.handleClick } key={ index } />
 			)
 		})
 		return (
 			<div className="calc-box">
-				<p>{ this.state.sum }</p>
+				<p>{ this.state.num }</p>
 				{ btnArray }
 			</div>
 		)
